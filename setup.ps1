4fd0b63b47e86e70327ff137e92518c770382e6d
Timo M. Staudinger <TimoSta@users.noreply.github.com>
2019-03-26 22:01:32 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -35,8 +35,6 @@ git config --global user.email "foo@bar.com"
 git config --global user.name "Timo Staudinger"
 New-Item C:\git -type directory
 
-# TODO: Copy ShareX config
-# TODO: Set ShareX proxy
 # TODO: Copy yarn & npm config
 # TODO: Copy Hyper config
 # TODO: Copy VS Code config
