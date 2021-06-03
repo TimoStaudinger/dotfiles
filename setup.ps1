361878263c8abf6d66e1ff3d9d8cdae8d0378119
Timo M. Staudinger <5621996+TimoStaudinger@users.noreply.github.com>
2021-06-03 00:23:32 -0400

    Switch to winget

--- setup.ps1
+++ setup.ps1
@@ -1,61 +1,65 @@
-# Set Windows ENVs
+# Set Windows ENVs if required
 # - HTTP_PROXY
 # - HTTPS_PROXY
 # - NO_PROXY
 
-# Set up Chocolatey for package management
-Set-ExecutionPolicy RemoteSigned
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-
-refreshenv
-
-# Install applications (general)
-choco install googlechrome -y
-choco install firefox -y
-choco install adobereader -y
-choco install 7zip -y
-choco install vlc -y
-choco install 1password -y
-choco install spotify -y
-
-# Install applications (development)
-choco install vcbuildtools -y
-choco install openjdk -y
-choco install python2 -y
-choco install vscode -y
-choco install notepadplusplus -y
-choco install nvm -y
-choco install yarn -y
-choco install git -y
-choco install poshgit -y
-choco install inkscape -y
-choco install visioviewer -y
-choco install dotnetcore-sdk -y
-
-choco install dbeaver -y
-choco install camunda-modeler -y
-
-# Install applications (others)
-choco install google-backup-and-sync -y
-choco install nordvpn -y
-choco install discord -y
-choco install steam -y
-choco install uplay -y
-choco install epicgameslauncher -y
-choco install parsec -y
-
-choco install geforce-experience -y
-
-# Install from Windows Store
-# - TranslucentTB
-# - Windows Terminal
-
-# Install non-Chocolatey software
-# - Adobe Creative Cloud
-# - Snagit
+# Install applications: general
+# =============================
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id AgileBits.1Password
+winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox
+winget install -e --id Adobe.AdobeAcrobatReaderDC
+winget install -e --id 7zip.7zip
+winget install -e --id VideoLAN.VLC
+winget install -e --id Spotify.Spotify
+winget install -e --id TranslucentTB.TranslucentTB
+winget install -e --id TechSmith.Snagit
 
 # Install fonts
 # - Dank Mono (https://dank.sh/releases/)
+
+# [optional]
+winget install -e --id Google.BackupAndSync
+winget install -e --id NordVPN.NordVPN
+winget install -e --id ParsecCloudInc.Parsec
+
+# Install applications: creative
+# ==============================
+winget install -e --id Microsoft.VisualStudio.2019.BuildTools
+winget install -e --id AdoptOpenJDK.OpenJDK
+winget install -e --id Python.Python
+winget install -e --id OpenJS.Nodejs
+winget install -e --id Yarn.Yarn
+winget install -e --id Git.Git
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Notepad++.Notepad++
+winget install -e --id Inkscape.Inkscape
+winget install -e --id dbeaver.dbeaver
+
+# Install manually
+# - poshgit
+# - Adobe creative cloud
+
+# [optional]
+# Install manually
+# - camunda-modeler
+
+# Install applications: entertainment
+# ===================================
+winget install -e --id Discord.Discord
+winget install -e --id Blizzard.BattleNet
+winget install -e --id Valve.Steam
+winget install -e --id Ubisoft.Connect
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id ParsecCloudInc.Parsec
+
+# [optional]
+winget install -e --id Nvidia.GeForceExperience
+
+# Configuration
+# =============
 
 refreshenv
 
@@ -68,11 +72,6 @@ New-Item C:\git -type directory
 
 # TODO: Copy yarn & npm config
 
-# Set up Node.js
-# - Configure proxy: nvm proxy ip:port
-nvm install 12.18.3
-nvm use 12.18.3
-
 refreshenv
 
 # Install Python packages
@@ -81,18 +80,24 @@ pip install --upgrade httpie
 
 # Enable VS Code settings sync
 
-# Activate Windows Subsystem for Linux
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-# Reboot
-wsl --set-default-version 2
-# If setting the wsl version to 2 fails, install the wsl2 kernel by hand, and retry:
-# https://docs.microsoft.com/en-us/windows/wsl/wsl2-kernel
-# Install Ubuntu from Windows Store
-
 # Set up PowerShell
 # - Clone psaliases
 # - Copy profile
 
-# Show hidden files
-# Show file extensions
+# Configure Windows
+# - Show hidden files
+# - Show file extensions
+
+# Activate Windows Subsystem for Linux
+# ====================================
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# Reboot
+
+wsl --set-default-version 2
+# If setting the wsl version to 2 fails, install the wsl2 kernel by hand, and retry:
+# https://docs.microsoft.com/en-us/windows/wsl/wsl2-kernel
+
+winget install -e --id Canonical.Ubuntu
