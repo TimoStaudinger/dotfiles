a2688d0cfa32a56e7b5156d7e7ca8a0e3926332f
Timo M. Staudinger <5621996+TimoStaudinger@users.noreply.github.com>
2021-07-05 13:41:32 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,5 @@ $aliasesDir = "C:\psaliases\dist"
 # Load all .ps1 files located in $aliasesDir
 Get-ChildItem "${aliasesDir}\*.ps1" | % {.$_}
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
 # load posh-git
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
